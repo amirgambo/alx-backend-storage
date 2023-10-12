0888da9bf108d3467b74e3f79d2c9298379144a2
amirgambo <gambo.amir@yahoo.com>
2023-10-12 15:24:20 +0000

    MySQL advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,6 @@
--- Calculate the lifespan based on the 'formed' and 'split' columns
--- The lifespan is calculated as years from 'formed' to 'split'
--- Use 2022 as the reference year
-
-SELECT band_name, 
-       IFNULL(DATEDIFF(
-           IF(splitted > 0, split, '2022-01-01'),
-           formed), 0) as lifespan
+-- Lists bands using Glam rock style, ranked by longevity
+SELECT  band_name,
+        IFNULL(split,2022) - IFNULL(formed,0) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%GLAM ROCK%'
-ORDER BY lifespan DESC;
+WHERE style like '%Glam rock%'
+ORDER BY 2 DESC; 
